ddfe65d525541107462959982d2f7dbcac61d1e0
PrestSQuare <tanaka@prest-square.jp>
2021-03-21 21:44:00 +0900

    Initial Commit

---
+++ gitTest2/gitTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  gitTest2App.swift
+//  gitTest2
+//
+//  Created by Tanaka Kenta on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct gitTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
